a473e08e7ea67f4a698e2bc05090296fe563fc3b
llj764480217 <llj764480217@yeah.net>
2019-09-24 11:35:27 +0800

    add a table Users

---
+++ dbsql/dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE Users
+(
+	username nvarchar(8) not null,
+	password varchar(20) not null,
+	phonenumber varchar(11) not null primary key,
+	sex nvarchar(1) not null check(sex = '男' or sex = '女') ,
+	address nvarchar(30),
+)
